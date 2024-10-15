558d60bf681416188568532c903bd85ac740c495
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2024-10-15 10:27:39 -0300

    fix: bento grid responsive

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -34,7 +34,7 @@ export const BentoGridItem = ({
   return (
     <Card
       className={cn(
-        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-none p-4 gap-4 marker:flex flex-col min-h-fit h-full',
+        'row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-none p-4 gap-4 marker:flex flex-col',
         className,
       )}
     >
