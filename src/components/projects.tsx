9756b9c8e9655ec81d79aa56df14008b27fffae6
john0x01 <personal.joaomarcelo.rocha@gmail.com>
2024-10-24 01:09:25 -0300

    feat: add delivery app photo

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -246,7 +246,7 @@ const data = [
   },
   {
     title: 'Burger Delivery',
-    src: 'https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: '/projects/delivery-cover.png',
     tags: ['apps'],
     href: '/projects/corelli',
     description:
